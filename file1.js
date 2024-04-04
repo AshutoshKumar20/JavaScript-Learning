22a34b539066ea31e095eddd40d711e224eb7df0
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-04-04 17:34:51 +0530

    Day-02 (DataTypes)

--- file1.js
+++ file1.js
@@ -1,2 +1,28 @@
-var firstName = "Ashu";
-console.log(firstName);
+// Variables
+
+// var firstName = "Ashutosh";
+// console.log(firstName);
+
+// Datatypes
+
+// var string = "Ashu";
+// console.log(typeof string);
+
+// var number = 12;
+// console.log(typeof number);
+
+// var bool = 12;
+// if (bool == string) {
+//     console.log("It is true")
+// } else {
+//     console.log("It is a false value")
+// }
+
+// var ashu;
+// console.log(ashu);
+
+// var fullName = "Ashutosh";
+// console.log(typeof +fullName);
+
+// var fullName = "12";
+// console.log(parseInt(typeof fullName))
