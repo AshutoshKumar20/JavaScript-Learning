988b55bf48c6ab583edaa4ad4aa2d48247c0b616
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-04-07 23:18:18 +0530

    Arithmetic Operators Completed

--- file1.js
+++ file1.js
@@ -44,3 +44,23 @@ var num1 = 12;
 var num2 = 15;
 var addition = num1 + num2;
 console.log(addition);
+
+var num1 = 12;
+var num2 = 15;
+var subtraction = num1 - num2;
+console.log(subtraction);
+
+var num1 = 12;
+var num2 = 15;
+var product = num1 * num2;
+console.log(product);
+
+var num1 = 12;
+var num2 = 15;
+var division = num1 / num2;
+console.log(division);
+
+var num1 = 12;
+var num2 = 15;
+var modulus = num1 % num2;
+console.log(modulus);
