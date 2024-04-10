486839accf3b406326ef730f5211513ab52b7ed6
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-04-10 22:31:01 +0530

    Question Practice

--- file1.js
+++ file1.js
@@ -89,6 +89,15 @@
 //     console.log("Not Equal")
 // }
 
+var str1 = "1";
+var str2 = "2";
+
+if (str1 === str2) {
+    console.log("Equal")
+} else {
+    console.log("Not Equal")
+}
+
 //Logical
 
 // &&(AND OPERATOR)
