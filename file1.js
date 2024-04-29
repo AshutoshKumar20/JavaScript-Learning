eef9509bdaf076c55c3cf015df6a7083be9d47fb
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-04-29 21:30:42 +0530

    while loop started

--- file1.js
+++ file1.js
@@ -258,3 +258,9 @@
 // }
 
 // While Loop
+
+var i = 0
+while (i = 0, i <= 20, i++) {
+
+}
+console.log(i)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-var str1 = "1";
-// var str2 = "2";
-
-// if (str1 === str2) {
-//     console.log("Equal")
-// } else {
-//     console.log("Not Equal")
-// }
+var i = 0
