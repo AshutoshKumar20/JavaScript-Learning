81376a51954b2784e1c1390260c8433a0c27affb
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-06-11 16:17:53 +0530

    Expressions started

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-for (var i = 1; i <= 5; i++) {
-//     var pattern = "";
-//     for (var j = 1; j <= i; j++) {
-//         pattern = pattern + " * "
-//     }
-//     console.log(pattern)
-// }
+hello
