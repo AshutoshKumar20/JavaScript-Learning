9e0dba9ae676924b17490a76895720b41468409b
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-06-28 21:45:32 +0530

    Remaining Questions to attempt

--- file1.js
+++ file1.js
@@ -444,6 +444,7 @@ for (let i = 0; i < studentNames.length; i++) {
 }
 console.log(average / studentNames.length);
 
+
 // 7. Write a JavaScript program that iterates integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers multiples of both three and five print "FizzBuzz".
 
 // for (i = 1; i <= 100; i++) {
