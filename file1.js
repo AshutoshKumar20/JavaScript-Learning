523a0fcee93d136de2cabfa7ec86b72ea9de4653
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-07-05 20:18:05 +0530

    Q9. Yet to be done

--- file1.js
+++ file1.js
@@ -512,7 +512,10 @@
 
 // 9. Write a JavaScript program to find the Armstrong numbers of 3 digits.
 // Note : An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
-// Click me to see the solution
+
+function armstrongNumber() {
+
+}
 
 // 10. Write a JavaScript program to construct the following pattern, using a nested for loop.
 
