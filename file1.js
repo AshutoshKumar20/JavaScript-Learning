a54a790a6be10ea6ab27ba09e6bd23d0f8eae29b
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-07-07 22:08:30 +0530

    Questions yet to be  complete

--- file1.js
+++ file1.js
@@ -514,7 +514,7 @@
 // Note : An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
 
 function armstrongNumber() {
-
+    for (i = 1; i <= 10; i++) { }
 }
 armstrongNumber()
 
