084de31ab0d970aa7fb973c6337c5017bafc530e
AshutoshKumar20 <ashutoshkumar44549@gmail.com>
2024-07-13 08:17:48 +0530

    Q12. Questions on Expressions and loops in Javascript done

--- file1.js
+++ file1.js
@@ -558,7 +558,14 @@
 
 // 12. Write a JavaScript program to sum 3 and 5 multiples under 1000.
 
-function sum() { }
+// var sum = 0;
+// for (i = 0; i < 1000; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//         sum = sum + i
+//     }
+
+// }
+// console.log(sum);
 
 // Functions
 
